57e941797da9bc140069c8e8f44df833813f8fb5
Vanderlei Mota <45984801+vmota86@users.noreply.github.com>
2019-08-25 03:08:32 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+# Configure the Azure Provider
+provider "azurerm"{
+    version         = "1.28.0"
+    client_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    client_secret   = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    tenant_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    subscription_id = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
